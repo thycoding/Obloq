ad862aad6b64c60288e8d2e98ec3b5528b58e9c4
thycoding <49593576+thycoding@users.noreply.github.com>
2020-09-23 22:56:02 +0800

    Update

--- Obloq.cpp
+++ Obloq.cpp
@@ -4,16 +4,16 @@ namespace Obloq {
 
     //%
     void obloqSetTxBufferSize(int size){
-        if(size > 500) {
-            size = 500;
+        if(size > 600) {
+            size = 600;
         }
         uBit.serial.setTxBufferSize(size);
     }
 
     //%
     void obloqSetRxBufferSize(int size){
-        if(size > 500) {
-            size = 500;
+        if(size > 600) {
+            size = 600;
         }
         uBit.serial.setRxBufferSize(size);
     }

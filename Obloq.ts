d3ba9df096de3e32f98585e5854f8223ced0a55f
thycoding <49593576+thycoding@users.noreply.github.com>
2020-11-27 10:12:29 +0800

    Update

--- Obloq.ts
+++ Obloq.ts
@@ -215,8 +215,8 @@ namespace Obloq {
             OBLOQ_SERIAL_RX,
             BaudRate.BaudRate9600
         )
-        obloqSetTxBufferSize(500)
-        obloqSetRxBufferSize(500)
+        obloqSetTxBufferSize(300)
+        obloqSetRxBufferSize(300)
         obloqWriteString("\r")
         item = serial.readString()
         OBLOQ_SERIAL_INIT = OBLOQ_BOOL_TYPE_IS_TRUE
